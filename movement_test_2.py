32582cc288b254000b557176cf29800032335db0
aap222 <aap222@lehigh.edu>
2022-05-14 21:19:38 -0400

    simulation working pretty well, next step is rigging everything up to sensors

--- movement_test_2.py
+++ movement_test_2.py
@@ -21,9 +21,8 @@ global RESCUE_CIRCLE
 RESCUE_CIRCLE = [0, -5]
 global LAST_POS
 LAST_POS = [-5, -5]
-FINAL_POS = [5, 5]
 
-# Globals for states of moving around the lab
+# Globals for moving around the lab
 global UP
 UP = 0
 global DOWN
@@ -61,15 +60,28 @@ global SQUARES
 SQUARES = len(GRID) * len(GRID[0])  # total # of squares in the lab
 #SQUARES = 5
 
-# fake data for testing
-global DUCKS
-DUCKS = [[0, 0], [4, 3]]
-global OBSTACLES
-OBSTACLES = [[-4, 3], [1, 1]]
-
 def print_grid():
     for row in GRID:
         print(row)
+
+def ultrasound_check():
+    global LAST_POS
+    limit = 30
+    ultrasound_reading = 100
+    if ultrasound_reading > limit: # if the ultrasound is saying we're far away from obstacle, do nothing
+        return False
+    
+    # otherwise we're too close to something
+    if STATE == UP:
+        mark_grid([LAST_POS[0] + 5, LAST_POS[1] + 6])
+    if STATE == DOWN:
+        mark_grid([LAST_POS[0] + 5, LAST_POS[1] + 4])
+    if STATE == LEFT:
+        mark_grid([LAST_POS[0] + 4, LAST_POS[1] + 5])
+    if STATE == RIGHT:
+        mark_grid([LAST_POS[0] + 6, LAST_POS[1] + 5])
+    return True
+
 
 def direction_check():
     global STATE
@@ -77,7 +89,13 @@ def direction_check():
     global Y_LIMIT
     global X_LIMIT
 
+    obstacle = ultrasound_check()
+    if obstacle:
+        return False
+
     if STATE == UP:
+        print("Last pos is: " + str(LAST_POS[1]))
+        print("Y limit: " + str(Y_LIMIT))
         if (LAST_POS[1]) == Y_LIMIT:
             return False
         return True
@@ -93,11 +111,6 @@ def direction_check():
         if (LAST_POS[0]) == X_LIMIT:
             return False
         return True
-
-# function that actually sends the move commands to the robot
-def go_to_coords(coords):
-    print("Going to coords: " + str(coords))
-    return True
 
 def move():
     global STATE
@@ -116,9 +129,8 @@ def move():
             STATE = DOWN
     if STATE == LEFT:
         LAST_POS[0] -= 1
-    go_to_coords(LAST_POS)
     print("New Position: " + str(LAST_POS))
-    mark_grid([LAST_POS[0] + 5, LAST_POS[1] + 5], SEARCHED)
+    mark_grid(LAST_POS)
     global EXPLORED
     EXPLORED += 1
 
@@ -132,72 +144,9 @@ def get_direction():
     if STATE == RIGHT:
         return "RIGHT"
 
-def mark_grid(POS, status):
+def mark_grid(POS):
     global GRID
-    GRID[POS[0]][POS[1]] = status
-
-def ultrasound_check():
-    global OBSTACLES
-    global LAST_POS
-    limit = 30  # limit for how close robot can get
-    detected_distance = 40
-    check = []
-
-    if STATE == UP:
-        check = [LAST_POS[0], LAST_POS[1] + 1]
-
-    if STATE == DOWN:
-        check = [LAST_POS[0], LAST_POS[1] - 1]
-
-    if STATE == LEFT:
-        check = [LAST_POS[0] - 1, LAST_POS[1]]
-
-    if STATE == RIGHT:
-        check = [LAST_POS[0] + 1, LAST_POS[1]]
-    
-    if check in OBSTACLES:
-        print("OBASTACLE DETECTED")
-        detected_distance = limit - 1
-    
-    if detected_distance <= limit:
-        print("Obstacle detected, going around")
-        if STATE == UP:
-            mark_grid([LAST_POS[0] + 5, LAST_POS[1] + 6], OBSTACLE)
-            go_to_coords([LAST_POS[0] - 1, LAST_POS[1]])
-            go_to_coords([LAST_POS[0] - 1, LAST_POS[1] + 2])
-            go_to_coords([LAST_POS[0], LAST_POS[1] + 2])
-            LAST_POS = [LAST_POS[0], LAST_POS[1] + 2]
-
-        if STATE == DOWN:
-            mark_grid([LAST_POS[0] + 5, LAST_POS[1] + 4], OBSTACLE)
-            go_to_coords([LAST_POS[0] - 1, LAST_POS[1]])
-            go_to_coords([LAST_POS[0] - 1, LAST_POS[1] - 2])
-            go_to_coords([LAST_POS[0], LAST_POS[1] - 2])
-            LAST_POS = [LAST_POS[0], LAST_POS[1] - 2]
-
-        if STATE == LEFT:
-            mark_grid([LAST_POS[0] + 4, LAST_POS[1] + 5], OBSTACLE)
-            go_to_coords([LAST_POS[0], LAST_POS[1] - 1])
-            go_to_coords([LAST_POS[0] - 2, LAST_POS[1] - 1])
-            go_to_coords([LAST_POS[0] - 2, LAST_POS[1]])
-            LAST_POS = [LAST_POS[0] - 2, LAST_POS[1]]
-
-        if STATE == RIGHT:
-            mark_grid([LAST_POS[0] + 6, LAST_POS[1] + 5], OBSTACLE)
-            go_to_coords([LAST_POS[0], LAST_POS[1] - 1])
-            go_to_coords([LAST_POS[0] + 2, LAST_POS[1] - 1])
-            go_to_coords([LAST_POS[0] + 2, LAST_POS[1]])
-            LAST_POS = [LAST_POS[0] + 2, LAST_POS[1]]
-
-    return True
-
-def duck_check():
-    global LAST_POS
-    global DUCKS
-
-    if LAST_POS in DUCKS:
-        print("Duck found!")
-        return True
+    GRID[POS[0] + 5][POS[1] + 5] = SEARCHED
 
 """
     Navigates around lab while using on-board camera with color filter to search for unique target
@@ -213,46 +162,29 @@ def duck_check():
         type - number indicating which type of target has been identified
 """
 def search():
-    go_to_coords(LAST_POS)
-    print("Starting Search at: " + str(LAST_POS))
+    print("Searching: starting at: " + str(LAST_POS))
     global STATE
     SPOTTED = False
-
-    wait_time = 0
-
-    # Big search loop that checks for several limits, like ultrasound and # of squares checked
     while not SPOTTED and EXPLORED < SQUARES:
-        if duck_check():
-            return True
-
-        # worked way to other end of lab
-        if LAST_POS[0] == FINAL_POS[0] and LAST_POS[1] == FINAL_POS[1]:
-            return False
-
-        # continue moving
-        #print("Making move number " + str(EXPLORED))
-
-        # check the ultrasound distance
-        distance = ultrasound_check()
-
-        # checks if next movement will move out of bounds
-        bounds = direction_check()
-        if bounds:
-            #print("Continuing movement in " + get_direction())
+        print("Making move number " + str(EXPLORED))
+        clear = direction_check()
+        if clear:
+            print("Continuing movement in " + get_direction())
             move()
-            time.sleep(wait_time)
-        if not bounds:
+            time.sleep(1)
+        if not clear:
             if STATE == UP:
                 STATE = RIGHT
             if STATE == RIGHT:
                 STATE = DOWN
             if STATE == DOWN:
                 STATE = RIGHT
-            #print("Changing movement to direction " + get_direction())
-            time.sleep(wait_time)
+            print("Changing movement to direction " + get_direction())
             continue
             
-    return False
+        
+    
+    return True
 
 """
     With the type of target having been identified, this function centers the target within its camera
@@ -265,17 +197,7 @@ def search():
         null - returning indicates the target has been captured
 """
 def capture():
-    global LAST_POS
-
     print("Capturing ducky")
-    if STATE == UP:
-        LAST_POS = [LAST_POS[0], LAST_POS[1] + 1]
-    if STATE == DOWN:
-        LAST_POS = [LAST_POS[0], LAST_POS[1] - 1]
-    if STATE == LEFT:
-        LAST_POS = [LAST_POS[0] - 1, LAST_POS[1]]
-    if STATE == UP:
-        LAST_POS = [LAST_POS[0] + 1, LAST_POS[1]]
 
 """
     Move robot with target in scoop back to the rescue circle.  After arriving at the rescue circle,
@@ -291,12 +213,9 @@ def capture():
 """
 def return_target():
     print("Returning to rescue circle at: " + str(RESCUE_CIRCLE))
-    go_to_coords(RESCUE_CIRCLE)
 
 if __name__ == "__main__":
-    continue_search = True
-    while continue_search:
-        continue_search = search()
-        capture()
-        return_target()
+    type = search()
+    capture()
+    return_target()
     print_grid()
